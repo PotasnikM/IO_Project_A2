71b9aab2014014070fc1b41235d2dbc59333f60c
FilipOpilka <filip.opilka@gmail.com>
2023-01-12 21:00:27 +0100

    Handling multiple input records

--- stronka/static/js/searchBar.js
+++ stronka/static/js/searchBar.js
@@ -1,16 +1,35 @@
+const addInputButton = document.getElementById("add-button");
+const inputContainer = document.getElementById("input-container");
+
 document.getElementById('search-button').addEventListener('click', search);
   // event.preventDefault(); // zapobiega przeładowaniu strony po kliknięciu przycisku
   // var searchInput = document.getElementById('search-input').value; // pobieramy wartość pola tekstowego
   // alert(searchInput)
 
+addInputButton.addEventListener("click", function() {
+  const newInput = document.createElement("input");
+  newInput.type = "text";
+  newInput.className = "search-input";
+  inputContainer.appendChild(newInput);
+});
+
 function search() {
-  let searchInput = document.getElementById('search-input').value;
-  // let searchInput =document.querySelectorAll(".search-input").values()
+  const searchInput =document.getElementsByClassName("search-input");
+  const itemsArray = [];
+
+  for(let i = 0; i < searchInput.length; i++) {
+      const item = {
+          name: searchInput.item(i).value
+      };
+      itemsArray.push(item);
+  }
+  alert(JSON.stringify(itemsArray));
+
   let temp = typeof searchInput;
   $.ajax({
       type: 'POST',
       url: '/search',
-      data: JSON.stringify({"name":searchInput}),
+      data: JSON.stringify(itemsArray),
       contentType: 'application/json',
       success: function(response) {
           // kod do wykonania po pomyślnym zakończeniu żądania
@@ -19,8 +38,3 @@ function search() {
       }
   });
 }
-// document.getElementById("add-button").addEventListener("click", function () {
-// // document.getElementById("innerdiv").innerHTML += "<h5><input type=\"text\" class=\"search-input\" " +
-// //                                                             "placeholder=\"Wpisz frazę wyszukiwania\"></h5>";});
-// document.getElementById("innerdiv").append(<h5><input type=\"text\" class=\"search-input\" " +
-//                                                             "placeholder=\"Wpisz frazę wyszukiwania\"></h5>)});
